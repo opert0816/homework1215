e7d941d5e5f85aa63c753588345659176d4c6dbc
xxx <xxbat1001@gmail.com>
2022-12-15 17:51:10 +0800

    1215

---
+++ Sample5_4.java
@@ -0,0 +1,22 @@
+public class Sample5_4 {
+
+    public static void main(String[] args){
+        try
+        {
+            int[]  test;
+            test=new int[5];
+            System.out.println("將值指定給text[10]");
+            test[10]=80;
+            System.out.println("將80指定給test[10]");
+
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("超過陣列的範圍");
+            System.out.println("發生了"+e+"這個例外");
+        }
+        finally {
+            System.out.println("最後一定會執行");
+        }
+        System.out.println("順利地執行完畢");
+    }
+}
---
+++ bb/sample5_4.java
@@ -0,0 +1,8 @@
+package bb;
+import pc.Car;
+public class sample5_4 {
+    public static void main(String[] args){
+        Car car1 = new Car();
+        car1.show();
+    }
+}
